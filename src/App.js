e5aed9494a51067e3aea19793281267d10ac7082
Akseli Keinänen <keinanenakseli@gmail.com>
2021-06-05 15:37:35 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Tulos from './components/Tulos';
+
+// Images
+import car1 from './imgs/car1.svg';
+import car2 from './imgs/car2.svg';
+import car3 from './imgs/car3.svg';
 
 function App() {
+
+  const [matka, setMatka] = useState('');
+  const [nopeus1, setNopeus1] = useState('');
+  const [nopeus2, setNopeus2] = useState('');
+  const [auto, setAuto] = useState("3");
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="text-center">
+        <h1 className="pt-5">Bensalaskuri</h1>
+        <p className="pt-3 pb-5">Valitse auto ja syötä arvot kenttiin</p>
+        <Row className="text-center">
+          <Col>
+            <label>
+              <input
+                type="radio"
+                name="auto"
+                id="autoA"
+                value="3"
+                checked={auto === "3"}
+                onChange={e => setAuto((e.target.value))}
+              />
+              <div className="icon">
+                <img src={car1} alt="Car 1" />
+                <p>Kulutus 3l/100km</p>
+              </div>
+            </label>
+          </Col>
+
+          <Col>
+            <label>
+              <input
+                type="radio"
+                name="auto"
+                id="autoB"
+                value="3.5"
+                checked={auto === "3.5"}
+                onChange={e => setAuto((e.target.value))}
+              />
+              <div className="icon">
+                <img src={car2} alt="Car 2" />
+                <p>Kulutus 3.5l/100km</p>
+              </div>
+            </label>
+          </Col>
+
+          <Col>
+            <label>
+              <input
+                type="radio"
+                name="auto"
+                id="autoC"
+                value="4"
+                checked={auto === "4"}
+                onChange={e => setAuto(e.target.value)}
+              />
+              <div className="icon">
+                <img src={car3} alt="Car 3" />
+                <p>Kulutus 4l/100kmh</p>
+              </div>
+            </label>
+          </Col>
+        </Row>
+
+        <Row className="mt-2">
+
+          <Col className="text-center mb-2">
+          <label htmlFor="matka">Syötä matkan pituus kilometreinä</label><br />
+          <input type="number" name="matka" autoComplete="off" value={matka} onChange={e => setMatka(e.target.value)} />
+          </Col>
+          <Col className="text-center mb-2">
+            <label htmlFor="nopeus">Syötä nopeus 1 (km/h)</label><br />
+            <input type="number" name="nopeus1" autoComplete="off" value={nopeus1} onChange={e => setNopeus1(e.target.value)} />
+          </Col>
+          <Col className="text-center">
+            <label htmlFor="nopeus">Syötä nopeus 2 (km/h)</label><br />
+            <input type="number" name="nopeus" autoComplete="off" value={nopeus2} onChange={e => setNopeus2(e.target.value)} />
+          </Col>
+
+        </Row>
+
+        <Row className="mt-5">
+
+        </Row>
+
+
+        <Tulos
+          className="tulos"
+          matka={matka}
+          auto={auto}
+          nopeus1={nopeus1}
+          nopeus2={nopeus2}
+        />
+
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+
+/* (kulutus2 * (Math.pow(1009, nopeus).toFixed(2) / Math.pow(1000, nopeus).toFixed(2))) / 100 */
---
+++ src/components/Tulos.js
@@ -0,0 +1,120 @@
+import { Col, Row } from 'react-bootstrap';
+import React, { useState, useRef } from 'react';
+import { Transition } from 'react-transition-group';
+
+const Tulos = ({ matka, auto, nopeus1, nopeus2 }) => {
+    const [showTulos, setShowTulos] = useState(false);
+    const nodeRef = useRef(null);
+
+    const laskeKulutus = (matka, nopeus, auto) => {
+
+        if (nopeus === 1) return auto / 100
+
+        let kilometriKulutus = (auto / 100 * Math.pow(1.009, nopeus - 1)).toFixed(10);
+
+        return (kilometriKulutus * matka).toFixed(3)
+
+    }
+
+    const vertaileLuku = (case1, case2) => {
+        return Math.max(case1, case2) - Math.min(case1, case2)
+    }
+
+    const esitaAika = (aika) => {
+        if(aika === Infinity) return 0;
+        return `${Math.floor(aika)}h ${Math.round(aika % 1 * 60)}m`;
+    }
+
+    const case1 = {
+        kesto: matka / nopeus1,
+        kulutus: laskeKulutus(matka, nopeus1, auto),
+    }
+    const case2 = {
+        kesto: matka / nopeus2,
+        kulutus: laskeKulutus(matka, nopeus2, auto),
+    }
+
+    if (!showTulos && matka && nopeus1 && nopeus2) {
+        setShowTulos(true);
+    } else if (showTulos && (!matka || !nopeus1 || !nopeus2)) {
+        setShowTulos(false);
+    }
+
+    const duration = 300;
+
+    const defaultStyle = {
+        transition: `opacity ${duration}ms ease-in-out`,
+        opacity: 0,
+    }
+
+    const transitionStyles = {
+        entering: { opacity: 1 },
+        entered: { opacity: 1 },
+        exiting: { opacity: 0 },
+        exited: { opacity: 0 },
+    };
+
+    return (
+
+        <>
+            <Transition
+                nodeRef={nodeRef}
+                in={!showTulos} timeout={500}
+            >
+                {state => (
+                    <div style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                    }}
+                    ref={nodeRef}
+                    >
+                        <h3 className="text-center">Syötä arvot kenttiin</h3>
+                    </div>
+                )}
+            </Transition>
+
+
+
+
+            <Transition 
+                in={showTulos} 
+                timeout={500} 
+                nodeRef={nodeRef}
+            >
+                {state => (
+                    <div style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                    }} 
+                    ref={nodeRef}>
+                        <Row>
+                            <Col className="text-center matka1 tuloslaatikko">
+
+                                {<h3>Kun ajat {matka}km matkan {nopeus1}km/h </h3>}
+
+                                <p>Matka kestää <b>{esitaAika(case1.kesto)}</b></p>
+                                <p>Bensaa kuluu <b>{case1.kulutus}</b></p>
+                            </Col>
+                            <Col className="text-center matka2 tuloslaatikko">
+                                <h3>Kun ajat {matka}km matkan {nopeus2}km/h </h3>
+                                <p>Matka kestää <b>{esitaAika(case2.kesto)}</b></p>
+                                <p>Bensaa kuluu <b>{case2.kulutus}</b> litraa</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="text-center tulos tuloslaatikko">
+                                <h3>Nopeammalla vaihtoehdolla:</h3>
+                                <ul className="d-inline-block text-left">
+                                    <li><p>Olet perillä {esitaAika(vertaileLuku(case1.kesto, case2.kesto))} nopeammin</p></li>
+                                    <li><p>Bensaa kuluu {(vertaileLuku(case1.kulutus, case2.kulutus)).toFixed(3)} litraa enemmän</p></li>
+                                </ul>
+                            </Col>
+                        </Row>
+                    </div>
+                )}
+            </Transition>
+        </>
+    )
+}
+
+export default Tulos;
